05853e5d8b4528c03f27925e04bacf0df766bac4
Ramazan Ozer <ozerramazan991@gmail.com>
2024-04-09 12:09:56 +0300

    Initial Commit

---
+++ The Card War/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  The Card War
+//
+//  Created by Ramazan Ozer on 9.04.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ The Card War/The_Card_WarApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Card_WarApp.swift
+//  The Card War
+//
+//  Created by Ramazan Ozer on 9.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct The_Card_WarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
